fb84d1664305d315b9779d3bb6c726087cceb5bd
mallory-scotton <mallory.scotton@epitech.eu>
2025-04-11 11:38:51 +0200

    add: adding renderer logics

--- Raytracer/Renderer/Renderer.cpp
+++ Raytracer/Renderer/Renderer.cpp
@@ -3,6 +3,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include "Renderer/Renderer.hpp"
 #include "Renderer/Scene.hpp"
+#include <cmath>
 
 ///////////////////////////////////////////////////////////////////////////////
 // Namespace Ray
@@ -100,7 +101,8 @@ String Renderer::Options::GetTonemapShaderDefinitions(void) const
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-Renderer::Renderer(void)
+Renderer::Renderer(SharedPtr<Scene> scene)
+    : mScene(scene)
 {
     InitGPUDataBuffers();
     InitFBOs();
@@ -183,8 +185,143 @@ void Renderer::InitGPUDataBuffers(void)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+void Renderer::InitPathTraceFBO(void)
+{
+    glGenFramebuffers(1, &mPathTraceFBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, mPathTraceFBO);
+
+    glGenTextures(1, &mPathTraceTexture);
+    glBindTexture(GL_TEXTURE_2D, mPathTraceTexture);
+    glTexImage2D(
+        GL_TEXTURE_2D, 0, GL_RGBA32F,
+        mTileWidth, mTileHeight, 0,
+        GL_RGBA, GL_FLOAT, 0
+    );
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFramebufferTexture2D(
+        GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+        GL_TEXTURE_2D, mPathTraceTexture, 0
+    );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void Renderer::InitPathTraceLowResFBO(void)
+{
+    glGenFramebuffers(1, &mPathTraceFBOLowRes);
+    glBindFramebuffer(GL_FRAMEBUFFER, mPathTraceFBOLowRes);
+
+    glGenTextures(1, &mPathTraceTextureLowRes);
+    glBindTexture(GL_TEXTURE_2D, mPathTraceTextureLowRes);
+    glTexImage2D(
+        GL_TEXTURE_2D, 0, GL_RGBA32F,
+        mWindowSize.x * mPixelRatio, mWindowSize.y * mPixelRatio, 0,
+        GL_RGBA, GL_FLOAT, 0
+    );
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFramebufferTexture2D(
+        GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+        GL_TEXTURE_2D, mPathTraceTextureLowRes, 0
+    );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void Renderer::InitAccumulationFBO(void)
+{
+    glGenFramebuffers(1, &mAccumulationFBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, mAccumulationFBO);
+
+    glGenTextures(1, &mAccumulationTexture);
+    glBindTexture(GL_TEXTURE_2D, mAccumulationTexture);
+    glTexImage2D(
+        GL_TEXTURE_2D, 0, GL_RGBA32F,
+        mRenderSize.x, mRenderSize.y, 0,
+        GL_RGBA, GL_FLOAT, 0
+    );
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFramebufferTexture2D(
+        GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+        GL_TEXTURE_2D, mAccumulationTexture, 0
+    );
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void Renderer::InitOutputFBO(void)
+{
+    glGenFramebuffers(1, &mOutputFBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, mOutputFBO);
+
+    glGenTextures(1, &mTileOutputTexture[0]);
+    glBindTexture(GL_TEXTURE_2D, mTileOutputTexture[0]);
+    glTexImage2D(
+        GL_TEXTURE_2D, 0, GL_RGBA32F,
+        mRenderSize.x, mRenderSize.y, 0,
+        GL_RGBA, GL_FLOAT, 0
+    );
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    glGenTextures(1, &mTileOutputTexture[1]);
+    glBindTexture(GL_TEXTURE_2D, mTileOutputTexture[1]);
+    glTexImage2D(
+        GL_TEXTURE_2D, 0, GL_RGBA32F,
+        mRenderSize.x, mRenderSize.y, 0,
+        GL_RGBA, GL_FLOAT, 0
+    );
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    glFramebufferTexture2D(
+        GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+        GL_TEXTURE_2D, mTileOutputTexture[mCurrentBuffer], 0
+    );
+}
+
+///////////////////////////////////////////////////////////////////////////////
 void Renderer::InitFBOs(void)
-{}
+{
+    mSampleCounter = 1;
+    mCurrentBuffer = 0;
+    mFrameCounter = 1;
+
+    mRenderSize = mOptions.renderResolution;
+    mWindowSize = mOptions.windowResolution;
+
+    mTileWidth = mOptions.tileWidth;
+    mTileHeight = mOptions.tileHeight;
+
+    mInvTileCount = static_cast<float>(mTileWidth / mRenderSize.x);
+    mInvTileCount = static_cast<float>(mTileHeight / mRenderSize.y);
+
+    mTileCount.x = std::ceil(static_cast<float>(mRenderSize.x / mTileWidth));
+    mTileCount.y = std::ceil(static_cast<float>(mRenderSize.y / mTileHeight));
+
+    mTile.x = -1;
+    mTile.y = mTileCount.y - 1;
+
+    InitPathTraceFBO();
+    InitPathTraceLowResFBO();
+    InitAccumulationFBO();
+    InitOutputFBO();
+
+    // TODO: Add denoiser
+
+    RAY_INFO("Window Resolution: " << mWindowSize);
+    RAY_INFO("Render Resolution: " << mRenderSize);
+    RAY_INFO("Preview Resolution: " << Vec2i(
+        mWindowSize.x * mPixelRatio, mWindowSize.y * mPixelRatio
+    ));
+    RAY_INFO("Tile Size: " << Vec2i(mTileWidth, mTileHeight));
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 void Renderer::InitShaders(void)
@@ -221,7 +358,67 @@ void Renderer::InitShaders(void)
 
 ///////////////////////////////////////////////////////////////////////////////
 void Renderer::Render(void)
-{}
+{
+    if (mDirty)
+    {
+        glBindFramebuffer(GL_FRAMEBUFFER, mPathTraceFBOLowRes);
+        glViewport(
+            0, 0, mWindowSize.x * mPixelRatio, mWindowSize.y * mPixelRatio
+        );
+
+        mPathTraceShaderLowRes->Use();
+        mQuad.Draw();
+        mPathTraceShaderLowRes->StopUsing();
+
+        mDirty = false;
+    }
+    else
+    {
+        glBindFramebuffer(GL_FRAMEBUFFER, mPathTraceFBO);
+        glViewport(0, 0, mTileWidth, mTileHeight);
+        glBindTexture(GL_TEXTURE_2D, mAccumulationTexture);
+
+        mPathTraceShader->Use();
+        mQuad.Draw();
+        mPathTraceShader->StopUsing();
+
+        glBindFramebuffer(GL_FRAMEBUFFER, mAccumulationFBO);
+        glViewport(
+            mTileWidth * mTile.x,
+            mTileHeight * mTile.y,
+            mTileWidth, mTileHeight
+        );
+        glBindTexture(GL_TEXTURE_2D, mPathTraceTexture);
+
+        mOutputShader->Use();
+        mQuad.Draw();
+        mOutputShader->StopUsing();
+
+        glBindFramebuffer(GL_FRAMEBUFFER, mOutputFBO);
+        glFramebufferTexture2D(
+            GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+            GL_TEXTURE_2D, mTileOutputTexture[mCurrentBuffer], 0
+        );
+        glViewport(0, 0, mRenderSize.x, mRenderSize.y);
+        glBindTexture(GL_TEXTURE_2D, mAccumulationTexture);
+
+        mTonemapShader->Use();
+        mQuad.Draw();
+        mTonemapShader->StopUsing();
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void Renderer::DrawToScreen(void)
+{
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glViewport(0, 0, mWindowSize.x, mWindowSize.y);
+    glBindTexture(GL_TEXTURE_2D, mTileOutputTexture[mCurrentBuffer]);
+
+    mTonemapShader->Use();
+    mQuad.Draw();
+    mTonemapShader->StopUsing();
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 void Renderer::Update(float deltaSeconds)
