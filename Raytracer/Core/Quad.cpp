3c2f36dd7f3504cc995f13878047c1d82a0855bf
mallory-scotton <mallory.scotton@epitech.eu>
2025-04-22 13:17:58 +0200

    add: adding quad

---
+++ Raytracer/Core/Quad.cpp
@@ -0,0 +1,57 @@
+///////////////////////////////////////////////////////////////////////////////
+// Dependencies
+///////////////////////////////////////////////////////////////////////////////
+#include "Core/Quad.hpp"
+#include "Core/Program.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// Namespace Ray
+///////////////////////////////////////////////////////////////////////////////
+namespace Ray
+{
+
+///////////////////////////////////////////////////////////////////////////////
+Quad::Quad(void)
+{
+    glGenVertexArrays(1, &m_vao);
+    glGenBuffers(1, &m_vbo);
+    glBindVertexArray(m_vao);
+    glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+
+    static const float vertices[] =
+    {
+        -1.0f, 1.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, 0.0f, 0.0f,
+        1.0f, -1.0f, 1.0f, 0.0f,
+        -1.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, -1.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 1.0f, 1.0f
+    };
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(
+        0, 2, GL_FLOAT, GL_FALSE,
+        4 * sizeof(GLfloat), (GLvoid*)0
+    );
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(
+        1, 2, GL_FLOAT, GL_FALSE,
+        4 * sizeof(GLfloat), (GLvoid*)(2 * sizeof(GLfloat))
+    );
+
+    glBindVertexArray(0);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void Quad::Draw(Program* shader)
+{
+    shader->Use();
+    glBindVertexArray(m_vao);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+    shader->StopUsing();
+}
+
+} // namespace Ray
