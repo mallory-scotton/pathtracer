6cc79b25646a7c52e29764210e6178309c8d5299
mallory-scotton <mallory.scotton@epitech.eu>
2025-05-01 16:36:28 +0200

    fix: fixing UniquePtr not having deleters

--- Raytracer/Config/Types.hpp
+++ Raytracer/Config/Types.hpp
@@ -89,7 +89,8 @@ template <typename T> using SharedPtr = std::shared_ptr<T>;
 template <typename T> using WeakPtr = std::weak_ptr<T>;
 
 ///////////////////////////////////////////////////////////////////////////////
-template <typename T> using UniquePtr = std::unique_ptr<T>;
+template <typename T, typename D = std::default_delete<T>>
+using UniquePtr = std::unique_ptr<T, D>;
 
 ///////////////////////////////////////////////////////////////////////////////
 template <typename T> using Vector = std::vector<T>;
