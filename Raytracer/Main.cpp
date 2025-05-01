acb0d2bf54164a9a9008e427dd3f45c207f3184b
mallory-scotton <mallory.scotton@epitech.eu>
2025-05-01 12:11:05 +0200

    remove: removing ImGui from the main

--- Raytracer/Main.cpp
+++ Raytracer/Main.cpp
@@ -8,10 +8,6 @@
 #include "Core/Renderer.h"
 #include "Components/Scene.h"
 #include "GL/gl3w.h"
-#include "ImGui/ImGuizmo.h"
-#include "ImGui/imgui.h"
-#include "ImGui/imgui_impl_opengl3.h"
-#include "ImGui/imgui_impl_sdl.h"
 #include "ImGui/tinydir.h"
 #include "Loaders/GLTFLoader.h"
 #include "Loaders/Loader.hpp"
@@ -146,80 +142,10 @@ void Render() {
     glViewport(0, 0, renderOptions.windowResolution.x,
                renderOptions.windowResolution.y);
     renderer->Present();
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
 void Update(float secondsElapsed) {
-    keyPressed = false;
-
-    if (!ImGui::GetIO().WantCaptureMouse && ImGui::IsAnyMouseDown() &&
-        !ImGuizmo::IsOver()) {
-        if (ImGui::IsMouseDown(0)) {
-            ImVec2 mouseDelta = ImGui::GetMouseDragDelta(0, 0);
-            scene->camera->OffsetOrientation(mouseDelta.x, mouseDelta.y);
-            ImGui::ResetMouseDragDelta(0);
-        } else if (ImGui::IsMouseDown(1)) {
-            ImVec2 mouseDelta = ImGui::GetMouseDragDelta(1, 0);
-            scene->camera->SetRadius(mouseSensitivity * mouseDelta.y);
-            ImGui::ResetMouseDragDelta(1);
-        } else if (ImGui::IsMouseDown(2)) {
-            ImVec2 mouseDelta = ImGui::GetMouseDragDelta(2, 0);
-            scene->camera->Strafe(mouseSensitivity * mouseDelta.x,
-                                  mouseSensitivity * mouseDelta.y);
-            ImGui::ResetMouseDragDelta(2);
-        }
-        scene->dirty = true;
-    }
-
     renderer->Update(secondsElapsed);
-}
-
-void EditTransform(const float* view, const float* projection, float* matrix) {
-    static ImGuizmo::OPERATION mCurrentGizmoOperation(ImGuizmo::TRANSLATE);
-    static ImGuizmo::MODE mCurrentGizmoMode(ImGuizmo::WORLD);
-
-    if (ImGui::RadioButton("Translate",
-                           mCurrentGizmoOperation == ImGuizmo::TRANSLATE)) {
-        mCurrentGizmoOperation = ImGuizmo::TRANSLATE;
-    }
-
-    ImGui::SameLine();
-    if (ImGui::RadioButton("Rotate",
-                           mCurrentGizmoOperation == ImGuizmo::ROTATE)) {
-        mCurrentGizmoOperation = ImGuizmo::ROTATE;
-    }
-
-    ImGui::SameLine();
-    if (ImGui::RadioButton("Scale",
-                           mCurrentGizmoOperation == ImGuizmo::SCALE)) {
-        mCurrentGizmoOperation = ImGuizmo::SCALE;
-    }
-
-    float matrixTranslation[3], matrixRotation[3], matrixScale[3];
-    ImGuizmo::DecomposeMatrixToComponents(matrix, matrixTranslation,
-                                          matrixRotation, matrixScale);
-    ImGui::InputFloat3("Tr", matrixTranslation);
-    ImGui::InputFloat3("Rt", matrixRotation);
-    ImGui::InputFloat3("Sc", matrixScale);
-    ImGuizmo::RecomposeMatrixFromComponents(matrixTranslation, matrixRotation,
-                                            matrixScale, matrix);
-
-    if (mCurrentGizmoOperation != ImGuizmo::SCALE) {
-        if (ImGui::RadioButton("Local", mCurrentGizmoMode == ImGuizmo::LOCAL)) {
-            mCurrentGizmoMode = ImGuizmo::LOCAL;
-        }
-
-        ImGui::SameLine();
-        if (ImGui::RadioButton("World", mCurrentGizmoMode == ImGuizmo::WORLD)) {
-            mCurrentGizmoMode = ImGuizmo::WORLD;
-        }
-    }
-
-    ImGuiIO& io = ImGui::GetIO();
-    ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
-    ImGuizmo::Manipulate(view, projection, mCurrentGizmoOperation,
-                         mCurrentGizmoMode, matrix, NULL, NULL);
 }
 
 void MainLoop(void* arg) {
@@ -227,7 +153,6 @@ void MainLoop(void* arg) {
 
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
-        ImGui_ImplSDL2_ProcessEvent(&event);
         if (event.type == SDL_QUIT) {
             done = true;
         }
@@ -253,272 +178,6 @@ void MainLoop(void* arg) {
                 done = true;
             }
         }
-    }
-
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplSDL2_NewFrame(loopdata.mWindow);
-    ImGui::NewFrame();
-    ImGuizmo::SetOrthographic(false);
-
-    ImGuizmo::BeginFrame();
-    {
-        ImGui::Begin("Settings");
-
-        ImGui::Text("Samples: %d ", renderer->GetSampleCount());
-
-        ImGui::BulletText("LMB + drag to rotate");
-        ImGui::BulletText("MMB + drag to pan");
-        ImGui::BulletText("RMB + drag to zoom in/out");
-        ImGui::BulletText("CTRL + click on a slider to edit its value");
-
-        if (ImGui::Button("Save Screenshot")) {
-            SaveFrame("./img_" + to_string(renderer->GetSampleCount()) +
-                      ".png");
-        }
-
-        // Scenes
-        std::vector<const char*> scenes;
-        for (int i = 0; i < sceneFiles.size(); ++i)
-            scenes.push_back(sceneFiles[i].c_str());
-
-        if (ImGui::Combo("Scene", &sampleSceneIdx, scenes.data(),
-                         scenes.size())) {
-            LoadScene(sceneFiles[sampleSceneIdx]);
-            SDL_RestoreWindow(loopdata.mWindow);
-            SDL_SetWindowSize(loopdata.mWindow,
-                              renderOptions.windowResolution.x,
-                              renderOptions.windowResolution.y);
-            int w, h;
-            SDL_GL_GetDrawableSize(loopdata.mWindow, &w, &h);
-            renderOptions.windowResolution.x = w;
-            renderOptions.windowResolution.y = h;
-            InitRenderer();
-        }
-
-        // Environment maps
-        std::vector<const char*> envMapsList;
-        for (int i = 0; i < envMaps.size(); ++i)
-            envMapsList.push_back(envMaps[i].c_str());
-
-        if (ImGui::Combo("EnvMaps", &envMapIdx, envMapsList.data(),
-                         envMapsList.size())) {
-            scene->AddEnvMap(envMaps[envMapIdx]);
-        }
-
-        bool optionsChanged = false;
-        bool reloadShaders = false;
-
-        optionsChanged |= ImGui::SliderFloat("Mouse Sensitivity",
-                                             &mouseSensitivity, 0.001f, 1.0f);
-
-        if (ImGui::CollapsingHeader("Render Settings")) {
-            optionsChanged |=
-                ImGui::SliderInt("Max Spp", &renderOptions.maxSpp, -1, 256);
-            optionsChanged |=
-                ImGui::SliderInt("Max Depth", &renderOptions.maxDepth, 1, 10);
-
-            reloadShaders |= ImGui::Checkbox("Enable Russian Roulette",
-                                             &renderOptions.enableRR);
-            reloadShaders |= ImGui::SliderInt("Russian Roulette Depth",
-                                              &renderOptions.RRDepth, 1, 10);
-            reloadShaders |=
-                ImGui::Checkbox("Enable Roughness Mollification",
-                                &renderOptions.enableRoughnessMollification);
-            optionsChanged |= ImGui::SliderFloat(
-                "Roughness Mollification Amount",
-                &renderOptions.roughnessMollificationAmt, 0, 1);
-            reloadShaders |= ImGui::Checkbox("Enable Volume MIS",
-                                             &renderOptions.enableVolumeMIS);
-        }
-
-        if (ImGui::CollapsingHeader("Environment")) {
-            reloadShaders |= ImGui::Checkbox("Enable Uniform Light",
-                                             &renderOptions.enableUniformLight);
-
-            Vec3 uniformLightCol =
-                Vec3f::Pow(renderOptions.uniformLightCol, 1.0 / 2.2);
-            optionsChanged |=
-                ImGui::ColorEdit3("Uniform Light Color (Gamma Corrected)",
-                                  (float*)(&uniformLightCol), 0);
-            renderOptions.uniformLightCol = Vec3f::Pow(uniformLightCol, 2.2);
-
-            reloadShaders |= ImGui::Checkbox("Enable Environment Map",
-                                             &renderOptions.enableEnvMap);
-            optionsChanged |=
-                ImGui::SliderFloat("Enviornment Map Intensity",
-                                   &renderOptions.envMapIntensity, 0.1f, 10.0f);
-            optionsChanged |=
-                ImGui::SliderFloat("Enviornment Map Rotation",
-                                   &renderOptions.envMapRot, 0.0f, 360.0f);
-            reloadShaders |=
-                ImGui::Checkbox("Hide Emitters", &renderOptions.hideEmitters);
-            reloadShaders |= ImGui::Checkbox("Enable Background",
-                                             &renderOptions.enableBackground);
-            optionsChanged |= ImGui::ColorEdit3(
-                "Background Color", (float*)&renderOptions.backgroundCol, 0);
-            reloadShaders |= ImGui::Checkbox(
-                "Transparent Background", &renderOptions.transparentBackground);
-        }
-
-        if (ImGui::CollapsingHeader("Tonemapping")) {
-            ImGui::Checkbox("Enable Tonemap", &renderOptions.enableTonemap);
-
-            if (renderOptions.enableTonemap) {
-                ImGui::Checkbox("Enable ACES", &renderOptions.enableAces);
-                if (renderOptions.enableAces)
-                    ImGui::Checkbox("Simple ACES Fit",
-                                    &renderOptions.simpleAcesFit);
-            }
-        }
-
-        if (ImGui::CollapsingHeader("Denoiser")) {
-            ImGui::Checkbox("Enable Denoiser", &renderOptions.enableDenoiser);
-            ImGui::SliderInt("Number of Frames to skip",
-                             &renderOptions.denoiserFrameCnt, 5, 50);
-        }
-
-        if (ImGui::CollapsingHeader("Camera")) {
-            float fov = Math::Degrees(scene->camera->fov);
-            float aperture = scene->camera->aperture * 1000.0f;
-            optionsChanged |= ImGui::SliderFloat("Fov", &fov, 10, 90);
-            scene->camera->SetFov(fov);
-            optionsChanged |=
-                ImGui::SliderFloat("Aperture", &aperture, 0.0f, 10.8f);
-            scene->camera->aperture = aperture / 1000.0f;
-            optionsChanged |= ImGui::SliderFloat(
-                "Focal Distance", &scene->camera->focalDist, 0.01f, 50.0f);
-            ImGui::Text("Pos: %.2f, %.2f, %.2f", scene->camera->position.x,
-                        scene->camera->position.y, scene->camera->position.z);
-        }
-
-        if (ImGui::CollapsingHeader("Objects")) {
-            bool objectPropChanged = false;
-
-            std::vector<std::string> listboxItems;
-            for (int i = 0; i < scene->meshInstances.size(); i++) {
-                listboxItems.push_back(scene->meshInstances[i].name);
-            }
-
-            // Object Selection
-            ImGui::ListBoxHeader("Instances");
-            for (int i = 0; i < scene->meshInstances.size(); i++) {
-                bool is_selected = selectedInstance == i;
-                if (ImGui::Selectable(listboxItems[i].c_str(), is_selected)) {
-                    selectedInstance = i;
-                }
-            }
-            ImGui::ListBoxFooter();
-
-            ImGui::Separator();
-            ImGui::Text("Materials");
-
-            // Material Properties
-            Material* mat =
-                &scene->materials[scene->meshInstances[selectedInstance]
-                                      .materialID];
-
-            // Gamma correction for color picker. Internally, the renderer uses
-            // linear RGB values for colors
-            Vec3 albedo = Vec3f::Pow(mat->baseColor, 1.0 / 2.2);
-            objectPropChanged |= ImGui::ColorEdit3("Albedo (Gamma Corrected)",
-                                                   (float*)(&albedo), 0);
-            mat->baseColor = Vec3f::Pow(albedo, 2.2);
-
-            objectPropChanged |=
-                ImGui::SliderFloat("Metallic", &mat->metallic, 0.0f, 1.0f);
-            objectPropChanged |=
-                ImGui::SliderFloat("Roughness", &mat->roughness, 0.001f, 1.0f);
-            objectPropChanged |= ImGui::SliderFloat(
-                "SpecularTint", &mat->specularTint, 0.0f, 1.0f);
-            objectPropChanged |=
-                ImGui::SliderFloat("Subsurface", &mat->subsurface, 0.0f, 1.0f);
-            objectPropChanged |= ImGui::SliderFloat(
-                "Anisotropic", &mat->anisotropic, 0.0f, 1.0f);
-            objectPropChanged |=
-                ImGui::SliderFloat("Sheen", &mat->sheen, 0.0f, 1.0f);
-            objectPropChanged |=
-                ImGui::SliderFloat("SheenTint", &mat->sheenTint, 0.0f, 1.0f);
-            objectPropChanged |=
-                ImGui::SliderFloat("Clearcoat", &mat->clearcoat, 0.0f, 1.0f);
-            objectPropChanged |= ImGui::SliderFloat(
-                "ClearcoatGloss", &mat->clearcoatGloss, 0.0f, 1.0f);
-            objectPropChanged |=
-                ImGui::SliderFloat("SpecTrans", &mat->specTrans, 0.0f, 1.0f);
-            objectPropChanged |=
-                ImGui::SliderFloat("Ior", &mat->ior, 1.001f, 2.0f);
-
-            int mediumType = (int)mat->mediumType;
-            if (ImGui::Combo("Medium Type", &mediumType,
-                             "None\0Absorb\0Scatter\0Emissive\0")) {
-                reloadShaders = true;
-                objectPropChanged = true;
-                mat->mediumType = mediumType;
-            }
-
-            if (mediumType != Material::NONE) {
-                Vec3 mediumColor = Vec3f::Pow(mat->mediumColor, 1.0 / 2.2);
-                objectPropChanged |=
-                    ImGui::ColorEdit3("Medium Color (Gamma Corrected)",
-                                      (float*)(&mediumColor), 0);
-                mat->mediumColor = Vec3f::Pow(mediumColor, 2.2);
-
-                objectPropChanged |= ImGui::SliderFloat(
-                    "Medium Density", &mat->mediumDensity, 0.0f, 5.0f);
-
-                if (mediumType == Material::SCATTER)
-                    objectPropChanged |=
-                        ImGui::SliderFloat("Medium Anisotropy",
-                                           &mat->mediumAnisotropy, -0.9f, 0.9f);
-            }
-
-            int alphaMode = (int)mat->alphaMode;
-            if (ImGui::Combo("Alpha Mode", &alphaMode, "Opaque\0Blend")) {
-                reloadShaders = true;
-                objectPropChanged = true;
-                mat->alphaMode = alphaMode;
-            }
-
-            if (alphaMode != Material::OPAQUE)
-                objectPropChanged |=
-                    ImGui::SliderFloat("Opacity", &mat->opacity, 0.0f, 1.0f);
-
-            // Transforms
-            ImGui::Separator();
-            ImGui::Text("Transforms");
-            {
-                float viewMatrix[16];
-                float projMatrix[16];
-
-                auto io = ImGui::GetIO();
-                scene->camera->ComputeViewProjectionMatrix(
-                    viewMatrix, projMatrix,
-                    io.DisplaySize.x / io.DisplaySize.y);
-                Mat4x4f xform =
-                    scene->meshInstances[selectedInstance].transform;
-
-                EditTransform(viewMatrix, projMatrix, (float*)&xform);
-
-                if (memcmp(&xform,
-                           &scene->meshInstances[selectedInstance].transform,
-                           sizeof(float) * 16)) {
-                    scene->meshInstances[selectedInstance].transform = xform;
-                    objectPropChanged = true;
-                }
-            }
-
-            if (objectPropChanged) scene->RebuildInstances();
-        }
-
-        scene->renderOptions = renderOptions;
-
-        if (optionsChanged) scene->dirty = true;
-
-        if (reloadShaders) {
-            scene->dirty = true;
-            renderer->ReloadShaders();
-        }
-
-        ImGui::End();
     }
 
     double presentTime = SDL_GetTicks();
@@ -617,33 +276,21 @@ int main(int argc, char** argv)
 
     // Initialize OpenGL loader
 #if GL_VERSION_3_2
-#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
     bool err = gl3wInit() != 0;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-    bool err = glewInit() != GLEW_OK;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-    bool err = gladLoadGL() == 0;
-#endif
+
     if (err) {
         fprintf(stderr, "Failed to initialize OpenGL loader!\n");
         return 1;
     }
 #endif
 
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    (void)io;
+    if (!InitRenderer())
+    {
+        return 1;
+    }
 
-    ImGui_ImplSDL2_InitForOpenGL(loopdata.mWindow, loopdata.mGLContext);
-
-    ImGui_ImplOpenGL3_Init(glsl_version);
-
-    ImGui::StyleColorsDark();
-    if (!InitRenderer()) return 1;
-
-    while (!done) {
+    while (!done)
+    {
         MainLoop(&loopdata);
     }
 
@@ -651,10 +298,6 @@ int main(int argc, char** argv)
     delete scene;
 
     // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplSDL2_Shutdown();
-    ImGui::DestroyContext();
-
     SDL_GL_DeleteContext(loopdata.mGLContext);
     SDL_DestroyWindow(loopdata.mWindow);
     SDL_Quit();
