ebd231b6419bf6da1ea3cb628c6d1954b2271055
mallory-scotton <mallory.scotton@epitech.eu>
2025-05-03 13:35:05 +0200

    add: adding tests for the GUi plugin

--- Raytracer/Plugins/Gui/GuiPlugin.cpp
+++ Raytracer/Plugins/Gui/GuiPlugin.cpp
@@ -2,6 +2,9 @@
 // Dependencies
 ///////////////////////////////////////////////////////////////////////////////
 #include "Plugins/Gui/GuiPlugin.hpp"
+#include "Core/Context.hpp"
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_internal.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 // Namespace Ray
@@ -10,8 +13,15 @@ namespace Ray
 {
 
 ///////////////////////////////////////////////////////////////////////////////
+const float GuiPlugin::MOUSE_SENSITIVITY = 0.01f;
+
+///////////////////////////////////////////////////////////////////////////////
 GuiPlugin::GuiPlugin(void)
-{}
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 GuiPlugin::~GuiPlugin()
@@ -21,11 +31,83 @@ GuiPlugin::~GuiPlugin()
 void GuiPlugin::Update(float deltaSeconds)
 {
     RAY_UNUSED(deltaSeconds);
+    Context& ctx = Context::GetInstance();
+
+    ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    if (!ImGui::GetIO().WantCaptureMouse && ImGui::IsAnyMouseDown())
+    {
+        if (ImGui::IsMouseDown(0))
+        {
+            ImVec2 mouseDelta = ImGui::GetMouseDragDelta(0, 0);
+            ctx.scene->camera->OffsetOrientation(mouseDelta.x, mouseDelta.y);
+            ImGui::ResetMouseDragDelta(0);
+        }
+        else if (ImGui::IsMouseDown(1))
+        {
+            ImVec2 mouseDelta = ImGui::GetMouseDragDelta(1, 0);
+            ctx.scene->camera->SetRadius(MOUSE_SENSITIVITY * mouseDelta.y);
+            ImGui::ResetMouseDragDelta(1);
+        }
+        else if (ImGui::IsMouseDown(2))
+        {
+            ImVec2 mouseDelta = ImGui::GetMouseDragDelta(2, 0);
+            ctx.scene->camera->Strafe(
+                MOUSE_SENSITIVITY * mouseDelta.x,
+                MOUSE_SENSITIVITY * mouseDelta.y
+            );
+            ImGui::ResetMouseDragDelta(2);
+        }
+        ctx.scene->dirty = true;
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 void GuiPlugin::PreRender(void)
-{}
+{
+    ImGuiIO& io = ImGui::GetIO();
+    Context& ctx = Context::GetInstance();
+
+    if (io.DisplaySize.x <= 0.f || io.DisplaySize.y <= 0.f || ctx.shutdown)
+    {
+        return;
+    }
+
+    ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->WorkPos);
+    ImGui::SetNextWindowSize(viewport->WorkSize);
+    ImGui::SetNextWindowViewport(viewport->ID);
+
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+
+    ImGuiWindowFlags window_flags =
+        ImGuiWindowFlags_NoDocking |
+        ImGuiWindowFlags_NoTitleBar |
+        ImGuiWindowFlags_NoCollapse |
+        ImGuiWindowFlags_NoResize |
+        ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoBringToFrontOnFocus |
+        ImGuiWindowFlags_NoNavFocus |
+        ImGuiWindowFlags_NoBackground;
+
+    ImGui::Begin("DockSpaceHost", nullptr, window_flags);
+    ImGui::PopStyleVar(3);
+
+    ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f),
+        ImGuiDockNodeFlags_PassthruCentralNode
+    );
+
+    ImGui::End();
+
+    ImGui::Begin("Settings");
+    ImGui::Text("Samples: %d ", ctx.renderer->GetSampleCount());
+    ImGui::End();
+
+    ImGui::ShowDemoWindow();
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 void GuiPlugin::Render(void)
