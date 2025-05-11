5b38dbea6d815fd2227edcdf9cf03142528ff550
mallory-scotton <mallory.scotton@epitech.eu>
2025-05-12 00:08:39 +0200

    add: adding infinitep plane primitive

---
+++ Raytracer/Objects/InfinitePlane.cpp
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////
+// Dependencies
+///////////////////////////////////////////////////////////////////////////////
+#include "Objects/InfinitePlane.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+// Namespace Ray::Objects
+///////////////////////////////////////////////////////////////////////////////
+namespace Ray::Objects
+{
+
+///////////////////////////////////////////////////////////////////////////////
+InfinitePlane::InfinitePlane(void)
+    : APrimitiveObject("plane")
+{
+    GenerateGeometry();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void InfinitePlane::GenerateGeometry(void)
+{
+    m_vertices.clear();
+    m_normals.clear();
+
+    const float limits = 1000000.0f;
+
+    Vec4f v0(-limits, 0.0f, -limits, 1.0f);
+    Vec4f v1( limits, 0.0f, -limits, 1.0f);
+    Vec4f v2( limits, 0.0f,  limits, 1.0f);
+    Vec4f v3(-limits, 0.0f,  limits, 1.0f);
+
+    Vec4f normal(0.0f, 1.0f, 0.0f, 0.0f);
+
+    m_vertices.push_back(v0);
+    m_normals.push_back(normal);
+    m_vertices.push_back(v1);
+    m_normals.push_back(normal);
+    m_vertices.push_back(v2);
+    m_normals.push_back(normal);
+
+    m_vertices.push_back(v0);
+    m_normals.push_back(normal);
+    m_vertices.push_back(v2);
+    m_normals.push_back(normal);
+    m_vertices.push_back(v3);
+    m_normals.push_back(normal);
+}
+
+} // namespace Ray::Objects
