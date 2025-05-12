4899cca1578d30223a5965eef32534b6c46ed9e3
mallory-scotton <mallory.scotton@epitech.eu>
2025-05-12 19:25:20 +0200

    add: adding new factories

--- Raytracer/Factories/PrimitiveFactory.cpp
+++ Raytracer/Factories/PrimitiveFactory.cpp
@@ -18,6 +18,9 @@ PrimitiveFactory::PrimitiveFactory(void)
     Register("torus", Objects::Torus::Constructor);
     Register("infinite_plane", Objects::InfinitePlane::Constructor);
     Register("fractal", Objects::Fractal::Constructor);
+    Register("cone", Objects::Cone::Constructor);
+    Register("cylinder", Objects::Cylinder::Constructor);
+    Register("mobius_strip", Objects::MobiusStrip::Constructor);
 }
 
 } // namespace Ray
